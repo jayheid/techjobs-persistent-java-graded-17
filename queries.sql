b8491845f9bd82b4b931eacd65a6f088b40ee2d4
jayheid <jp.heidbrink@gmail.com>
2024-11-09 12:45:34 -0600

    modified queries for task 4

--- queries.sql
+++ queries.sql
@@ -18,8 +18,8 @@ DROP TABLE job;
 
 --Part 4
 SELECT
-	skill.name
+	*
 FROM
 	skill
-    INNER JOIN job_skills ON (skill.id = job_skills.skills_id)
-ORDER BY skill.name ASC;
+    INNER JOIN job_skills ON skill.id = job_skills.skills_id
+ORDER BY name ASC;
